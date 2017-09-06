6f82db006a342222248c61a06cb337ce83e5c4c2
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-06 13:18:39 -0500

    servicio tecnico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,26 @@
-var estado=true;  
-setTimeout("ver()",900);  
-function ver (){  
-    estado=!estado;  if(estado==true)  texto1.style.visibility="visible";
-      else  texto1.style.visibility="hidden";  
-      setTimeout("ver()",900);  
+// var estado=true;  
+// setTimeout("ver()",900);  
+// function ver (){  
+//     estado=!estado;  if(estado==true)  texto1.style.visibility="visible";
+//       else  texto1.style.visibility="hidden";  
+//       setTimeout("ver()",900);  
+// }
+
+// ver();
+
+// seccion modal
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-ver();
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
